19cc99f6b3a557070be40c0ad7a4a0b90e177556
jennifertakagi <jennitakagi@gmail.com>
2021-02-22 16:20:47 -0300

    test: add unit test to Numbers

---
+++ test/chewiepay/numbers_test.exs
@@ -0,0 +1,21 @@
+defmodule Chewiepay.NumbersTest do
+  use ExUnit.Case
+
+  alias Chewiepay.Numbers
+
+  describe "sum_from_file/1" do
+    test "when there is a file with the given name, returns the sum of numbers" do
+      response = Numbers.sum_from_file("numbers")
+      expected_response = {:ok, %{result: 32}}
+
+      assert response == expected_response
+    end
+
+    test "when there is no file with the given name, returns an error" do
+      response = Numbers.sum_from_file("banana")
+      expected_response = {:error, %{message: "Invalid file!"}}
+
+      assert response == expected_response
+    end
+  end
+end
