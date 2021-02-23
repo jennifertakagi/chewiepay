33db05f6c42eb0609f70db9c2b184a74fa8ff958
jennifertakagi <jennitakagi@gmail.com>
2021-02-23 16:19:52 -0300

    feat: add configuration to use binary_id no DB

--- config/config.exs
+++ config/config.exs
@@ -18,6 +18,10 @@ config :chewiepay, ChewiepayWeb.Endpoint,
   pubsub_server: Chewiepay.PubSub,
   live_view: [signing_salt: "bRbnwVft"]
 
+config :chewiepay, Chewiepay.Repo,
+  migration_primary_key: [type: :binary_id],
+  migration_foreign_key: [type: :binary_id]
+
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
